22bf12da3645abbca9bdad298d766bf8f17f4f23
Jon Kessler <jon.kessler@appfolio.com>
2013-06-15 20:39:15 -0700

    refactoring

--- lib/gemsurance/runner.rb
+++ lib/gemsurance/runner.rb
@@ -7,23 +7,40 @@ module Gemsurance
     end
 
     def run
-      # discover what gems are in our bundle
+      bundled_gem_infos = retrieve_bundled_gem_infos
+
+      retrieve_vulnerability_data
+
+      add_vulnerability_data(bundled_gem_infos)
+
+      generate_report(bundled_gem_infos)
+
+      exit 1 if bundled_gem_infos.any? { |info| info.vulnerable? }
+    end
+
+  private
+
+    def retrieve_bundled_gem_infos
       puts "Retrieving gem version information..."
-      # Bundler.definition.validate_ruby!
+
       current_specs = Bundler.load.specs
       definition    = Bundler.definition(true)
       definition.resolve_remotely!
 
-      gem_infos = GemInfoRetriever.new(current_specs, definition).retrieve(:pre => @options[:pre])
+      GemInfoRetriever.new(current_specs, definition).retrieve(:pre => @options[:pre])
+    end
 
+    def retrieve_vulnerability_data
       puts "Retrieving latest vulnerability data..."
       if File.exists?('./tmp/vulnerabilities')
         g = Git.open('./tmp/vulnerabilities')
         g.pull
       else
-        _ = Git.clone('https://github.com/rubysec/ruby-advisory-db', './tmp/vulnerabilities')
+        Git.clone('https://github.com/rubysec/ruby-advisory-db', './tmp/vulnerabilities')
       end
+    end
 
+    def add_vulnerability_data(gem_infos)
       puts "Reading vulnerability data..."
       gem_infos.each do |gem_info|
         vulnerability_directory = "./tmp/vulnerabilities/gems/#{gem_info.name}"
@@ -38,7 +55,9 @@ module Gemsurance
           end
         end
       end
+    end
 
+    def generate_report(gem_infos)
       puts "Generating report..."
       output_data = Gemsurance.const_get(:"#{@formatter.to_s.capitalize}Formatter").new(gem_infos).format
 
@@ -46,7 +65,6 @@ module Gemsurance
         file.puts output_data
       end
       puts "Generated report #{@output_file}."
-      exit 1 if gem_infos.any? { |info| info.vulnerable? }
     end
   end
 end
