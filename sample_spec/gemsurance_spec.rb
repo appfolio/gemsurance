09a7131462c7ebedbc155bad751081ec7a1daecc
Sam Giffney <samuelgiffney@gmail.com>
2017-05-05 23:35:13 +1000

    Modernise sample spec

--- sample_spec/gemsurance_spec.rb
+++ sample_spec/gemsurance_spec.rb
@@ -1,8 +1,10 @@
-require 'spec_helper'
+require 'rails_helper'
 
 describe 'Check for vulnerabilities in Ruby gems' do
   specify 'Return value of gemsurance call should be zero' do
     `bundle exec gemsurance`
-    $?.to_i.zero?.should be_true, "One or more of your Ruby gems has a known security vulnerability. Check #{Rails.root}/gemsurance_report.html for more info."
+    expect($?.to_i.zero?).to be_truthy,
+      'One or more of your Ruby gems has a known security vulnerability. '\
+      "Check #{Rails.root}/gemsurance_report.html for more info."
   end
 end
